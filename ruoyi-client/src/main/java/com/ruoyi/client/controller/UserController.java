85ff0590f13eec573ea39b91d204a0e047a68654
wxl <1695607239@qq.com>
2023-09-18 11:45:52 +0800

    修改用户端静态资源访问

--- ruoyi-client/src/main/java/com/ruoyi/client/controller/UserController.java
+++ ruoyi-client/src/main/java/com/ruoyi/client/controller/UserController.java
@@ -5,6 +5,7 @@ import com.ruoyi.client.service.impl.UserServiceImpl;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.utils.StringUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,14 +14,18 @@ import java.util.Map;
 /**
  * @author 16956
  */
-@RestController
-@RequestMapping("/user")
+@Controller
+@RequestMapping("/client/user")
 public class UserController extends BaseController {
 
     @Resource
     private UserServiceImpl userService;
-
-    @PostMapping("/login")
+    @GetMapping("toLogin")
+    public String toLogin(){
+        return "user/login";
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
     public R checkLogin(@RequestBody UserLoginDTO userLoginDTO) {
       if (userLoginDTO == null){
           return R.fail("请输入邮箱和密码");
@@ -34,7 +39,8 @@ public class UserController extends BaseController {
         return userService.getUserByEmailAndPassword(userLoginDTO.getEmail(), userLoginDTO.getPassword());
     }
 
-    @PostMapping("/register")
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    @ResponseBody
     public R register(@RequestBody Map<String, String> info) {
         return userService.addUserToRegister(info);
     }
